4f92197df524f99318340c14587a27b98b377ce2
Christoph von Krüchten <cvk@webrunners.de>
2014-10-07 14:22:30 +0200

    Added solution for 3

---
+++ F#/problem3.fs
@@ -0,0 +1,11 @@
+﻿module problem3
+
+let rec factorize p n = 
+    if n = p then p
+    else
+        if n % p = 0L then factorize p (n/p)
+        else factorize (p + 1L) n
+
+let result = factorize 2L 600851475143L
+
+printfn "%A" result
