e1ad08cd50d9bf7de7e965f7ea232bb5edca7fb9
Christoph von Krüchten <cvk@webrunners.de>
2014-10-07 13:47:32 +0200

    Formatting

--- F#/problem2.fs
+++ F#/problem2.fs
@@ -1,6 +1,9 @@
 ﻿module problem2
 
 let fib = Seq.unfold (fun (a,b) -> Some( a+b, (b, a+b) ) ) (0,1)
-let result = fib |> Seq.takeWhile(fun x -> x < 4000000) |> Seq.filter(fun x -> x % 2 = 0) |> Seq.sum
+let result = fib 
+                |> Seq.takeWhile(fun x -> x < 4000000) 
+                |> Seq.filter(fun x -> x % 2 = 0) 
+                |> Seq.sum
 
 printfn "%A" result
