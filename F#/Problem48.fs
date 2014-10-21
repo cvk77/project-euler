8a761d684b2636ae901c33a0321ca31dd4f9fff6
Christoph von Krüchten <cvkkoeln@gmail.com>
2014-10-21 23:33:13 +0200

    Added #48

---
+++ F#/Problem48.fs
@@ -0,0 +1,22 @@
+﻿module Problem48
+
+// The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+//
+// Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+//
+// Answer: 9110846700
+
+
+let result = 
+
+    let s = { 1I..1000I }
+            |> Seq.map ( fun x -> bigint.Pow(x, int(x)) )
+            |> Seq.sum
+            |> string
+
+    s.Substring(s.Length-10)
+
+
+
+
+        
