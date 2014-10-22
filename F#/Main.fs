1d2259ef5d09d722f37abe85643640f221cbcc0d
Christoph von Krüchten <cvk@webrunners.de>
2014-10-22 10:33:09 +0200

    Added solution for #50

--- F#/Main.fs
+++ F#/Main.fs
@@ -52,4 +52,5 @@ open Tools.Test
 //printfn "Problem 47: %A" (Problem47.result |> shouldEqual 134043)
 //printfn "Problem 48: %A" (Problem48.result |> shouldEqual "9110846700")
 //printfn "Problem 49: %A" (Problem49.result |> shouldEqual "296962999629")
+//printfn "Problem 50: %A" (Problem50.result |> shouldEqual 997651)
 printfn "Problem 92: %A" (Problem92.result |> shouldEqual 8581146)
---
+++ F#/Problem50.fs
@@ -0,0 +1,28 @@
+﻿module Problem50
+
+// The prime 41, can be written as the sum of six consecutive primes:
+// 41 = 2 + 3 + 5 + 7 + 11 + 13
+// 
+// This is the longest sum of consecutive primes that adds to a prime below one-hundred.
+// 
+// The longest sum of consecutive primes below one-thousand that adds to a prime, contains 21 terms, and is equal to 953.
+// 
+// Which prime, below one-million, can be written as the sum of the most consecutive primes?
+//
+// Answer: 997651
+   
+open Tools.Prime
+
+let primes = { 2 .. 3931 } |> Seq.filter isPrime |> Seq.cache
+
+let getPrimeSequence n =
+        primes
+        |> Seq.filter (fun x -> x > n)
+        |> Seq.scan (fun (sum, count) x -> (sum + x, count + 1)) (n, 1)
+        |> Seq.filter (fun (sum, count) -> isPrime sum)
+        |> Seq.maxBy (fun (sum, count) -> count)
+ 
+let result = primes 
+                |> Seq.map getPrimeSequence 
+                |> Seq.maxBy (fun (sum, count) -> count)
+                |> fst
