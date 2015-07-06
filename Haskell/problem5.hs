068bda560ebb552d97b2534d600b0115d37c1dc9
Christoph von Krüchten <christoph@vonkruechten.de>
2015-07-06 23:38:39 +0200

    Added solutions for problem 7 and 8 in Haskell

--- Haskell/problem5.hs
+++ Haskell/problem5.hs
@@ -1,9 +1,9 @@
-module Euler5 where
+module Problem5 where
 
-{-
-2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
--}
+-- 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+-- What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+--
+-- Answer: 232792560
 
 import Test.Hspec
 
